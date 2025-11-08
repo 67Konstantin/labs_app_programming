fde755869ddc73b95a61228692bb3daf9acbd7d7
Константин <pilmeshok@gmail.com>
2025-11-09 02:20:32 +0300

    Добавил шрифты Шрифты есть основые и декоративные в папке assets/fonts

---
+++ lab3/main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.setWindowTitle("Приложение заметок")
+    window.resize(400, 300)
+    window.show()
+
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
